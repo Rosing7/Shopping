3e64aa05e4531354760d362fcb63c87c7afaf396
Siming <156222219+Rosing7@users.noreply.github.com>
2024-02-22 08:16:53 +0700

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rupp_;
+
+/**
+ *
+ * @author Admin
+ */
+
+public class Customer implements DiscountRate{
+    private String customerName;
+    private String customerType;
+
+    public Customer(String customerName, String customerType){
+        this.customerName = customerName;
+        this.customerType = customerType;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    @Override
+    public double getServiceMemberDiscount() {
+        switch (customerType){
+            case "Premium":
+                return 0.20;
+            case "Gold":
+                return 0.15;
+            case "Silver":
+                return 0.10;
+            default:
+                return 0;
+        }
+    }
+
+    @Override
+    public double getProductMemberDiscount() {
+        switch (customerType){
+            case "Premium":
+            case "Gold":
+            case "Silver":
+                return 0.10;
+            default:
+                return 0;
+        }
+    }
+}
