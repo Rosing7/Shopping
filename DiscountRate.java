9d26c69bbd8e5d390fabc5c80981cd423de3b02a
Siming <156222219+Rosing7@users.noreply.github.com>
2024-02-22 08:16:27 +0700

    Add files via upload

---
+++ DiscountRate.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rupp_;
+
+/**
+ *
+ * @author Admin
+ */
+public interface DiscountRate {
+    double getServiceMemberDiscount();
+    double getProductMemberDiscount();
+}
